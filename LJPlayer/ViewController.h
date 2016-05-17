a1555dc687a683c5c017da814e130090cff6cd29
iOS-李健 <jasonlee@108.cn>
2016-05-17 11:47:16 +0800

    Initial Commit

---
+++ LJPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LJPlayer
+//
+//  Created by JasonLee on 16/5/17.
+//  Copyright © 2016年 JasonLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LJPlayer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LJPlayer
+//
+//  Created by JasonLee on 16/5/17.
+//  Copyright © 2016年 JasonLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
